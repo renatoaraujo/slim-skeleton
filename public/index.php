dfeaf918ff8de6d0bfb240f6051b58d69c6bb9d0
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2016-06-03 20:05:25 -0300

    refactoring the code of application and the bootstrap class (not good or even finished but working.. need improvements)

--- public/index.php
+++ public/index.php
@@ -1,42 +1,28 @@
 <?php
 
-use \Slim\Container;
-use \Slim\App;
 use \Slim\Views\Twig;
 use \Slim\Views\TwigExtension;
 
 define('APPPATH', realpath('../'));
 require_once (APPPATH . "/vendor/autoload.php");
 $config = require_once (APPPATH . "/src/config/config.php");
+$routes = include_once (APPPATH . "/src/config/routes.php");
 
-$app = new App($config);
-$container = $app->getContainer();
+$app = new Skeleton\Library\Bootstrap($routes, $config);
 
-// ##
-// CREATE YOUR CUSTOM VARIABLES TO CALL ON ROUTES
-// ##
-$container['view'] = function ($c) {
+$app->injectDependecy('view', function ($c) {
     $view = new Twig(APPPATH . '/src/views', [
         // 'cache' => APPPATH . '/cache'
         'cache' => false
     ]);
     $view->addExtension(new TwigExtension($c['router'], $c['request']->getUri()));
     return $view;
-};
-
-// ##
-// INCLUDE DE ROUTES FILE AND RUN THE APPLICATION
-// ##
-$routes = include_once (APPPATH . "/src/config/routes.php");
-
-if (is_array($routes) && ! empty($routes)) {
-    foreach ($routes as $name => $route) {
-        if (isset($route['middleware']) && ! empty($route['middleware'])) {
-            $app->add($route['middleware']);
-        }
-        $app->$route['method']($route['url'], "{$route['callback']['controller']}:{$route['callback']['function']}")
-            ->setName($name);
-    }
-}
+});
 
 $app->run();
+
+// $debg = new \Skeleton\Library\Debug();
+// $debg->setLogPath('debug');
+// $debg->setLogPath(APPPATH . "/logs/debug.log");
+// $debg->setJsonFormat(false);
+// $debg->displayScreen();
--- src/skeleton/Controller/HomeController.php
+++ src/skeleton/Controller/HomeController.php
@@ -2,11 +2,13 @@
 namespace Skeleton\Controller;
 
 use Skeleton\Controller\SkeletonController;
+use \Psr\Http\Message\ServerRequestInterface;
+use \Psr\Http\Message\ResponseInterface;
 
 class HomeController extends SkeletonController
 {
 
-    public function testing($request, $response, $args)
+    public function testing(ServerRequestInterface $request, ResponseInterface $response, $args)
     {
         $params = [];
         return $this->ci->view->render($response, 'index.html', $params);
