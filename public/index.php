476f8aafabefb92148322bb5ee380f10feb415ce
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2016-06-03 22:25:58 -0300

    Service to initialize the twig view

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-use \Slim\Views\Twig;
-use \Slim\Views\TwigExtension;
-
 define('APPPATH', realpath('../'));
 require_once (APPPATH . "/vendor/autoload.php");
 $config = require_once (APPPATH . "/src/config/config.php");
@@ -10,19 +7,6 @@ $routes = include_once (APPPATH . "/src/config/routes.php");
 
 $app = new Skeleton\Library\Bootstrap($routes, $config);
 
-$app->injectDependecy('view', function ($c) {
-    $view = new Twig(APPPATH . '/src/views', [
-        // 'cache' => APPPATH . '/cache'
-        'cache' => false
-    ]);
-    $view->addExtension(new TwigExtension($c['router'], $c['request']->getUri()));
-    return $view;
-});
+$app->injectDependecy('view', new \Skeleton\Service\TwigViewService());
 
 $app->run();
-
-// $debg = new \Skeleton\Library\Debug();
-// $debg->setLogPath('debug');
-// $debg->setLogPath(APPPATH . "/logs/debug.log");
-// $debg->setJsonFormat(false);
-// $debg->displayScreen();
