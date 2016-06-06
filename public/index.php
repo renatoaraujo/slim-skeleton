77c7fd0c2748d80fb22ba6625913d4ba7de26bf3
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2016-06-06 10:11:34 -0300

    Removed useless line

--- public/index.php
+++ public/index.php
@@ -6,9 +6,6 @@ require_once (APPPATH . "/vendor/autoload.php");
 $config = require_once (APPPATH . "/app/config/config.php");
 $routes = include_once (APPPATH . "/app/config/routes.php");
 
-
-$asd = new \Skeleton\Middleware\HomeMiddleware();
-
 $app = new Skeleton\Library\Bootstrap($routes, $config);
 
 $app->injectDependecy([
