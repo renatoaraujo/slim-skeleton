abd49fff5355ce714d0f5479a2a1cecf2aa060e8
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2016-06-15 15:36:08 -0300

    Using unique html page to show error and error pages redesigned

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -3,54 +3,72 @@ defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV
 defined('APPPATH') || define('APPPATH', dirname(__DIR__));
 defined('ROOTPATH') || define('ROOTPATH', dirname(APPPATH));
 
-require_once (ROOTPATH . "/vendor/autoload.php");
+require_once(ROOTPATH . "/vendor/autoload.php");
 
-$settings = require_once (APPPATH . '/config/config.php');
-$routes = require_once (APPPATH . '/config/routes.php');
+$settings = require_once(APPPATH . '/config/config.php');
+$routes = require_once(APPPATH . '/config/routes.php');
 
 $app = new \Skeleton\Library\Bootstrap($routes, $settings);
 
 $app->injectDependecy([
-  'view' => '\Skeleton\Service\TwigViewService',
-  'em' => '\Skeleton\Service\DoctrineORMService',
-  'logger' => '\Skeleton\Service\MonologService',
-  'userService' => [
-    'service' => '\Skeleton\Service\UserService',
-    'dependency' => 'em',
-  ],
-  'errorHandler' => function($c) {
-    return function ($request, $response, $exception) use ($c) {
-
-      $c['logger']->critical($exception->getMessage());
-
-      $params = [];
-
-      if (APPLICATION_ENV === 'development' || APPLICATION_ENV === 'testing') {
-        $params = [
-          'error' => $exception->getMessage(),
-          'status' => '500 Internal server error'
-        ];
-      }
-
-      return $c['view']->render($response, '500.html', $params)
-      ->withStatus(500);
-    };
-  },
-  'notFoundHandler' => function ($c) {
-    return function ($request, $response) use ($c) {
-      $params = [
-        'status' => '404 not found',
-      ];
-      return $c['view']->render($response, '404.html', $params)
-      ->withStatus(404);
-    };
-  }
+    'view' => '\Skeleton\Service\TwigViewService',
+    'em' => '\Skeleton\Service\DoctrineORMService',
+    'logger' => '\Skeleton\Service\MonologService',
+    'userService' => [
+        'service' => '\Skeleton\Service\UserService',
+        'dependency' => 'em',
+    ],
+    'errorHandler' => function ($c) {
+        return function ($request, $response, $exception) use ($c) {
+            $c['logger']->critical($exception->getMessage());
+            $params = [
+                'error' => $exception->getMessage(),
+                'status' => [
+                    'code' => '500',
+                    'error' => 'Internal Server Error',
+                    'message' => 'message',
+                    'icon' => 'server'
+                ]
+            ];
+            return $c['view']->render($response, 'error.html', $params)
+                ->withStatus(500);
+        };
+    },
+    'notFoundHandler' => function ($c) {
+        return function ($request, $response) use ($c) {
+            $params = [
+                'status' => [
+                    'code' => '404',
+                    'error' => 'Page Not Found',
+                    'message' => 'The page you requested does not exist or were moved to another URL.',
+                    'icon' => 'map-signs',
+                ]
+            ];
+            return $c['view']->render($response, 'error.html', $params)
+                ->withStatus(404);
+        };
+    },
+    'notAllowedHandler' => function ($c) {
+        return function ($request, $response, $methods) use ($c) {
+            $params = [
+                'status' => [
+                    'code' => '405',
+                    'error' => 'Method Not Allowed',
+                    'message' => 'Method must be one of: ' . implode(', ', $methods),
+                    'icon' => 'gears'
+                ]
+            ];
+            return $c['view']->render($response, 'error.html', $params)
+                ->withHeader('Allow', implode(', ', $methods))
+                ->withStatus(405);
+        };
+    },
 ]);
 
 if (APPLICATION_ENV === 'development' || APPLICATION_ENV === 'testing') {
-  $app->addGenericMiddleware([
-    '\Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware'
-  ]);
+    $app->addGenericMiddleware([
+        '\Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware'
+    ]);
 }
 
 return $app;
